bd52d3ee73306ef794804b71993a20c7ee9d850f
Mohit Gupta <mohitgupta@pal-nat186-38-198.itap.purdue.edu>
2018-11-15 19:13:46 -0500

    final results along with partial completed report

---
+++ sgd.py
@@ -0,0 +1,77 @@
+import numpy as np
+import sys
+from util import *
+from graph_plotting import *
+
+def train_sgd(train_data, num_epochs, learning_rate, lamda, test_data):
+    data_size = len(train_data)
+    feature_length = train_data.shape[1]
+    weights = np.random.uniform(-0.1,0.1,[10,feature_length])
+    bias = np.ones((data_size,1))
+    prev_loss = 1000
+    batch_size_loss = 1000
+    epoch_axis = []
+    train_accuracy_axis = []
+    test_accuracy_axis = []
+    epoch=0
+    while epoch < num_epochs:
+        loss = np.zeros(10)
+        np.random.shuffle(train_data)
+        examples, labels, classifier_labels = get_features_labels(train_data, bias)
+        for i,example in enumerate(examples):
+            delta_weights = np.zeros((10,feature_length))
+            z = np.sum(weights*example, axis = 1)
+            y_pred = sigmoid(z)
+            delta_weights += learning_rate*np.outer(y_pred-classifier_labels[:,i],example)
+            loss+=classifier_labels[:,i]*np.log(y_pred+0.0000000001)+(1-classifier_labels[:,i])*np.log(1-y_pred+0.0000000001)
+            weights = weights - delta_weights - learning_rate*lamda*weights
+            if i%batch_size_loss==0:
+                loss = loss*-1.0/batch_size_loss + lamda*np.sum(np.square(weights), axis = 1)/2.0
+                loss = np.sum(loss)/10.0
+                
+                train_prediction, train_labels, train_accuracy = inference(train_data[:data_size], weights)
+                test_prediction, test_labels, test_accuracy = inference(test_data, weights)
+                '''
+                 for graph plotting
+                '''
+                epoch_axis.append(epoch)
+                train_accuracy_axis.append(train_accuracy)
+                test_accuracy_axis.append(test_accuracy)
+                
+                print "epoch ", epoch, ": Training loss: ", loss, " Training Accuracy: ", train_accuracy, " Test Accuracy: ", test_accuracy
+                delta_loss = prev_loss - loss
+                prev_loss = loss
+                loss = np.zeros(10)
+                epoch += 1
+                # print "delta loss ", delta_loss
+                if delta_loss > 0 and delta_loss < 0.0002 and epoch > 200:
+                    return weights, epoch_axis, train_accuracy_axis, test_accuracy_axis
+    return weights, epoch_axis, train_accuracy_axis, test_accuracy_axis
+
+def main(argv):
+    if len(argv) < 3:
+        print "Usage: python sgd.py [regularization? True/False] [Feature_Type? type1/type2] [path to DATA_FOLDER]"
+    else:
+        feature_type = argv[1]
+        path = argv[2]
+        train_data_size = 10000
+        epochs = 300
+        learning_rate = 0.001
+        if argv[0] == 'True':
+            lamda = 0.0001
+        else:
+            lamda = 0
+        
+        print "lamda is ", lamda, " feature_type is ", feature_type
+
+        train_data = preprocess(path + '/train-images-idx3-ubyte.gz', path + '/train-labels-idx1-ubyte.gz', feature_type)
+        test_data = preprocess(path + '/t10k-images-idx3-ubyte.gz', path + '/t10k-labels-idx1-ubyte.gz', feature_type)
+
+        gd_weights, epoch_axis, train_accuracy_axis, test_accuracy_axis = train_sgd(train_data[:10000], epochs, learning_rate, lamda, test_data)
+        # test_prediction, test_labels, test_accuracy = inference(test_data_type1, gd_weights)
+
+        plot_learning_curves('Epoch', epoch_axis, train_accuracy_axis, test_accuracy_axis, 'sgd-' + feature_type + '-reg-' + argv[0])
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- util.py
+++ util.py
@@ -1,18 +1,44 @@
 import numpy as np
 import struct
-# from f1_macro import calculate_macro_f1_score
+import sys
+import gzip
 
 def sigmoid(value):
+    value[value > 100] = 100
+    value[value < -100] = -100
     return 1/(1+np.exp(-value))
 
+def inference(test_data, weights):
+    data_size = len(test_data)
+    bias = np.ones((data_size,1))
+    examples, labels, classifier_labels = get_features_labels(test_data, bias)
+    prediction = np.ones(data_size, dtype = int)
+    correct = 0
+    for i, example in enumerate(examples):
+        activation_values = sigmoid(np.sum(weights*example, axis = 1))
+        prediction[i] = np.argmax(activation_values)
+        if prediction[i] == labels[i]:
+            correct += 1
+    accuracy = correct*1.0/data_size
+    return prediction, labels, accuracy
+    
 def read_file(filename):
-    with open(filename,'rb') as fp:
+    with gzip.open(filename,'rb') as fp:
         zero, data_type, dims = struct.unpack('>HBB', fp.read(4))
         shape = tuple(struct.unpack('>I', fp.read(4))[0] for d in range(dims))
         np_array = np.frombuffer(fp.read(), dtype=np.uint8).reshape(shape)
     return np_array
 
-def preprocess(image_file, label_file):
+def preprocess(image_file, label_file, feature_type):
+    if feature_type == 'type1':
+        return preprocess_type1(image_file, label_file)
+    elif feature_type == 'type2':
+        return preprocess_type2(image_file, label_file)
+    else:
+        print "invalid feature type"
+        return
+
+def preprocess_type1(image_file, label_file):
     images = read_file(image_file)
     labels = read_file(label_file)
     if (len(labels) > 10000):
@@ -23,35 +49,34 @@ def preprocess(image_file, label_file):
 
     labels = labels.reshape(-1,1)
     data = np.concatenate((images, labels), axis=1)
-#     np.random.shuffle(data)
+    np.random.shuffle(data)
     return data
 
-def get_true_label(digit, perceptron_type):
-    if digit == perceptron_type:
-        return 1
-    return -1
+def preprocess_type2(image_file, label_file):
+    images = read_file(image_file)
+    labels = read_file(label_file)
+    if (len(labels) > 10000):
+        labels = labels[:10000]
+        images = images[:10000]    
+    images = images/255.0
+    images_sample = np.zeros((10000, 14, 14))
+    for i,image in enumerate(images):
+        for j in range(14):
+            for k in range(14):
+                images_sample[i][j][k] = max(image[j*2][k*2], image[j*2][k*2+1],
+                                                 image[j*2+1][k*2], image[j*2+1][k*2+1])
+    images_sample = images_sample.reshape( (10000, 196))
+
+    labels = labels.reshape(-1,1)
+    data = np.concatenate((images_sample, labels), axis=1)
+    np.random.shuffle(data)
+    return data
 
 def get_features_labels(data, bias):
     examples = data[:,:-1]
     labels = data[:,-1]
+    classifier_labels = np.zeros((10, len(labels)))
+    for i in range(10):
+        classifier_labels[i][labels == i] = 1
     examples = np.append(examples, bias, 1)
-    return examples, labels
-
-def inference(test_data, weights):
-    data_size = len(test_data)
-    bias = np.ones((data_size,1))
-    examples, labels = get_features_labels(test_data, bias)
-    prediction = np.ones(data_size, dtype = int)
-    correct = 0
-    for i, example in enumerate(examples):
-        activation_values = np.sum(weights*example, axis = 1)
-        prediction[i] = np.argmax(activation_values)
-        if prediction[i] == labels[i]:
-            correct += 1
-#     print correct*1.0/data_size*100
-    return prediction, labels
-
-# def get_f1_score(test_data, weights):
-#     prediction, true_labels = inference(test_data, weights)
-#     f1_score = calculate_macro_f1_score(prediction, true_labels)
-#     return f1_score
+    return examples, labels, classifier_labels
